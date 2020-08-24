d026986ebb215543091c990334b56c1970b2a473
RichA <rich_ay@sbcglobal.net>
2020-08-23 22:55:21 -0500

    Added drag/drop/delete functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,81 @@ var saveTasks = function() {
 };
 
 //////////////////////////////////////////////////////////////////
+// Setup the lists to be "sortable", so we can reorder them and
+// move them between lists.
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+
+    // Define an array to store the updated task data.
+    var tempArr = [];
+
+    // Loop over the current set of children in the sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // Put the data in a new temporary array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log( "This is the tempArr: ", tempArr);
+
+    // Trim down list's ID to match the object property
+    var arrName = $(this)
+       .attr("id")
+       .replace("list-", "");
+
+    // Update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+
+///////////////////////////////////////////////////////////////////
+// Setup the ability to drop a task on/in the 'delete' zone
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+
+//////////////////////////////////////////////////////////////////
 // The user clicked on a task item, switch this to 'edit' mode
 $(".list-group").on("click", "p", function() {
   var text =  $(this)
